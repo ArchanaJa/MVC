2749c64ef566a38ae50a8ce8949dcf7c32ea1a37
ArchanaJa <archu5432@gmail.com>
2017-06-14 16:22:40 +0100

    Add files via upload

---
+++ MVCDemo1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(int id = 1)
+        {
+            ViewBag.NumberOfTimes = id;
+
+            return View();
+        }
+
+        public ActionResult Contact(int id=1)
+        {
+            ViewBag.Message = "Hello, you typed in the number " + id;
+
+            return View();
+        }
+    }
+}
